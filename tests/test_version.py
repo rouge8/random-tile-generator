d379c80c593d243e7990ff4b9a207a26b378e89a
Andy Freeland <andy@andyfreeland.net>
2024-06-08 10:39:43 -0700

    Initial commit

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,5 +1,5 @@
-import python_template
+import random_tile_generator
 
 
 def test_version():
-    assert python_template.__version__ is not None
+    assert random_tile_generator.__version__ is not None
